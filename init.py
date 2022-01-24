933173928682945ae33aab330c4f20ebe6cfb72a
sunrunner4kr@Hotmail.com <sunrunner4kr@hotmail.com>
2022-01-24 17:04:13 +0000

    update

--- init.py
+++ init.py
@@ -1,8 +1,9 @@
 from operator import attrgetter
+from socket import socket
 
 from flask import Flask, render_template, request, redirect, url_for
 from characters import Character
-from flask_sockets import Sockets
+from ws4py.client.threadedclient import WebSocketClient
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -10,6 +11,26 @@ app.secret_key = "hello"
 character_names = ["Shanko", "Saelwyn",
                    "Kaelar", "Owly", "Tree", "Gith", "Otadus"]
 players = []
+
+esp8266host = "ws://192.168.1.65:81/"
+
+
+class DummyClient(WebSocketClient):
+    def opened(self):
+        print("Websocket open")
+
+    def closed(self, code, reason=None):
+        print("Connection closed down", code, reason)
+
+    def received_message(self, m):
+        print(m)
+
+
+def findPlayer(name):
+    for player in players:
+        if player.name == name:
+            return player
+    return
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -22,23 +43,26 @@ def index():
     return redirect(url_for('dashboard'))
 
 
-@app.route("/dashboard", methods=['GET', 'POST'])
+@ app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
     if request.method == 'POST':
-        if request.form['button'] == 'add_player':
-            return redirect(url_for('add_character'))
-        elif request.form['button'] == 'sort':
-            players.sort(key=attrgetter('initiative'), reverse=True)
-        elif request.form['button'] == 'reset':
-            players.clear()
-        else:
-            pass  # unknown
+        if 'button' in request.form:
+            if request.form['button'] == 'add_player':
+                return redirect(url_for('add_character'))
+            elif request.form['button'] == 'sort':
+                players.sort(key=attrgetter('initiative'), reverse=True)
+            elif request.form['button'] == 'reset':
+                players.clear()
+            else:
+                pass  # unknown
+        if 'enable' in request.form:
+            Character.toggle_enabled(findPlayer(request.form['enable']))
         return render_template("dyn.html", content=players)
     elif request.method == 'GET':
         return render_template("dyn.html", content=players)
 
 
-@app.route("/add_character", methods=['GET', 'POST'])
+@ app.route("/add_character", methods=['GET', 'POST'])
 def add_character():
     if request.method == 'POST':
         selected = request.form.getlist('enabled-input')
